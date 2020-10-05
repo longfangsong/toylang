3f220a189797027c5cb921dcb676ffe12a314055
longfangsong <longfangsong@icloud.com>
2020-10-05 14:29:14 +0800

    use macro to define operator levels

--- src/parser/expression/bin_op/mod.rs
+++ src/parser/expression/bin_op/mod.rs
@@ -1,13 +1,65 @@
+use paste::paste;
+macro_rules! bin_op_level {
+    ($n: expr, $n_1: expr, $($op: expr)*) => {
+        paste! {
+            mod [<level $n>] {
+                use crate::parser::expression::bin_op::{
+                    self, BinOp,
+                    [<level $n_1>]::{self, [<higher_than_level $n_1>]}
+                };
+                use crate::parser::expression::rvalue::RValue;
+                use nom::branch::alt;
+                use nom::bytes::complete::tag;
+                use nom::character::complete::space0;
+                use nom::combinator::map;
+                use nom::multi::many1;
+                use nom::sequence::tuple;
+                use nom::IResult;
+
+                pub(in crate::parser::expression) fn [<higher_than_level $n>](
+                    code: &str,
+                ) -> IResult<&str, RValue> {
+                    alt((
+                        map([<level $n_1>]::parse, |binop| binop.into()),
+                        [<higher_than_level $n_1>],
+                    ))(code)
+                }
+
+                pub fn parse(code: &str) -> IResult<&str, BinOp> {
+                    map(
+                        tuple((
+                            [<higher_than_level $n>],
+                            many1(map(
+                                tuple((
+                                    space0,
+                                    alt((
+                                        $(
+                                            tag($op),
+                                        )*
+                                    )),
+                                    space0,
+                                   [<higher_than_level $n>],
+                                )),
+                                |(_, op, _, operand)| (op.to_string(), operand),
+                            )),
+                        )),
+                        |(first, rest)| bin_op::to_ast(first, rest),
+                    )(code)
+                }
+            }
+        }
+    };
+}
+
 // levels are same with C's Operator Precedence
-// todo: replace these with macros
-mod level10;
 mod level3;
-mod level4;
-mod level5;
-mod level6;
-mod level7;
-mod level8;
-mod level9;
+bin_op_level!(4, 3, "+" "-");
+bin_op_level!(5, 4, "<<" ">>");
+bin_op_level!(6, 5, "<=" "<" ">=" ">");
+bin_op_level!(7, 6, "==" "!=");
+bin_op_level!(8, 7, "&" "&");
+bin_op_level!(9, 8, "^" "^");
+bin_op_level!(10, 9, "|" "|");
 
 use crate::ir::calculate::{CalculateOperation, Operand};
 use crate::ir::{Calculate, Store, IR};
